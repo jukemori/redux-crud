95e86421abd1f6c19c1ad52f7fcaa4c57188d073
jukemori <jukemori@gmail.com>
2023-09-12 20:28:02 +0900

    axios fetching

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, ScrollView } from "react-native";
+import axios from "axios";
 
+const API_BASE_URL = "http://172.19.252.107:8000";
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = `${API_BASE_URL}/api/tasks`;
+
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setTasks(response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <ScrollView>
+        {tasks.map((task) => (
+          <View key={task.id}>
+            <Text>{task.title}</Text>
+            <Text>yooo</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
